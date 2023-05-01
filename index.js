83b3f200438e7bc04a6295fe4ea03cd177144932
Mk4Levi <120794789+Mk4Levi@users.noreply.github.com>
2023-05-01 13:20:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ fetch(
     // Use a default background image/author
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
 )`;
-    document.getElementById("author").textContent = `by: Dodi Achmad`;
+    document.getElementById("author").textContent = `by: Manthan`;
   });
 
 //
